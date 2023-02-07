dcc0118d55f10c6dd19df3088454eb4638a80cc2
Yaser <Yaser@MSI>
2023-02-07 18:58:56 +0100

    Add project files.

---
+++ Todos.App/Extension/ConversionExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Todos.App.Extension
+{
+	public static class ConversionExtensions
+	{
+		public static bool ToBool(this object? value) => value is not null && (bool)value;
+	}
+}
---
+++ Todos.App/NewFolder2/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace Todos.App.NewFolder2
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool Completed { get; set; }
+        
+
+    }
+}
